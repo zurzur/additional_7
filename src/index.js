097326319acc88d75b3f3ba5264b2d2d342a88f0
zurzur <@gmail.com>
2018-03-05 10:57:51 +0300

    sudoku/feature: 1/3 passed tests

--- src/index.js
+++ src/index.js
@@ -1,120 +1,179 @@
 module.exports = function solveSudoku(matrix) {
-
-    let solved = matrix;
-    let solvedNumbers = 0;
-    let steps = 0;
-
+let solved = [[],[],[],[],[],[],[],[],[]];
+let changed = true;
+let steps = 0;
 
 addSuggestions();
-while (solvedNumbers<81 && steps<1000){
-	updateSuggestions();
-	steps++;
-}
+solve(solved);
+
+
+
 output();
+
 return solved;
 
 
 
-function addSuggestions() {
+function solve(input){
+	
+	while (changed) {
+		changed = false;
+		steps ++;
+		for (let i = 0; i<9; i++) {
+			for (let j = 0; j < 9; j++) {
+				if (solved[i][j][1].length === 1) {addSolution(i,j,solved[i][j][1][0]);}
+				//isHiddenSingle(i,j);
+				updateSuggestions(i,j,solved[i][j][0]);
+			}
+		}
+		for (let i = 0; i<9; i++) {
+			for (let j = 0; j < 9; j++) {
+				//if (solved[i][j][1].length === 1) {addSolution(i,j,solved[i][j][1][0]);}
+				isHiddenSingle(i,j);
+				//updateSuggestions(i,j,solved[i][j][0]);
+			}
+		}
+	}
 
-	for ( let i = 0; i < 9; i++){
-		for (let j = 0; j < 9; j++){
-			if (solved[i][j]) {
-				solved[i][j] = [solved[i][j], []];
-				solvedNumbers++;
-			} else solved[i][j] = [solved[i][j], [1,2,3,4,5,6,7,8,9]];
+}
+
+function addSuggestions (){
+	for (let i = 0; i < 9; i++) {
+		for (let j = 0; j < 9; j++) {
+			if (matrix[i][j]) {
+				solved[i][j] = [matrix[i][j], [], 'solved'];
+			} else solved[i][j] = [matrix[i][j], [1,2,3,4,5,6,7,8,9], 'unsolved'];
 		}
 	}
 }
 
-function updateSuggestionsRow(row, number) {
-	for (let j = 0; j < 9; j++){
-		if (solved[row][j][1].length > 1 && solved[row][j][1].indexOf(number) > -1) {
-			solved[row][j][1].splice(solved[row][j][1].indexOf(number), 1);
+function addSolution(i, j, num){
+	solved[i][j] = [num, [], 'solved'];
+	changed = true;
+	updateSuggestions(i, j, num)
+}
+
+function updateSuggestions(row, col, num){
+	updateSuggestionsRow(row, num);
+	updateSuggestionsCol(col, num);
+	updateSuggestionsSection(row, col, num);
+}
+
+function updateSuggestionsRow(row, num){
+	for (let j = 0; j < 9; j++) {
+		if (solved[row][j][1].length > 1 && solved[row][j][1].indexOf(num) > -1) {
+			solved[row][j][1].splice(solved[row][j][1].indexOf(num), 1);
+			changed = true;
 		}
 	}
 }
 
-function updateSuggestionsColumn(column, number) {
-	for (let i = 0; i < 9; i++){
-		if (solved[i][column][1].length > 1 && solved[i][column][1].indexOf(number) > -1) {
-			solved[i][column][1].splice(solved[i][column][1].indexOf(number), 1);
+function updateSuggestionsCol(col, num){
+	for (let i = 0; i < 9; i++) {
+		if (solved[i][col][1].length > 1 && solved[i][col][1].indexOf(num) > -1) {
+			solved[i][col][1].splice(solved[i][col][1].indexOf(num), 1);
+			changed = true;
 		}
-	}	
+	}
 }
 
-function updateSuggestionsSection(row, column, number) {
-	let sectionFirstRowIndex = row/3|0;
+function updateSuggestionsSection(row, col, num){
+	let sectionFirstRowIndex = (row/3|0)*3;
 	let sectionLastRowIndex = sectionFirstRowIndex + 3;
-	let sectionFirstColumnIndex = column/3|0;
+	let sectionFirstColumnIndex = (col/3|0)*3;
 	let sectionLastColumnIndex = sectionFirstColumnIndex + 3;
-	let checked = 0;
-	let checkedRow;
-	let checkedColumn;
+
 	for (let i = sectionFirstRowIndex; i < sectionLastRowIndex; i++) {
 		for (let j = sectionFirstColumnIndex; j < sectionLastColumnIndex; j++) {
-			if(solved[i][j][0]){
-					for (let a = sectionFirstRowIndex; a < sectionLastRowIndex; a++) {
-						for (let b = sectionFirstColumnIndex; b < sectionLastColumnIndex; b++) {
-							solved[a][b][1].splice(solved[a][b][1].indexOf(solved[i][j][0]), 1);
-						}
-					}
+			if (solved[i][j][1].length > 1 && solved[i][j][1].indexOf(num) > -1) {
+				solved[i][j][1].splice(solved[i][j][1].indexOf(num), 1);
+				changed = true;
 			}
 		}
 	}
-	for (sectionFirstRowIndex; sectionFirstRowIndex < sectionLastRowIndex; sectionFirstRowIndex++) {
-		for (sectionFirstColumnIndex; sectionFirstColumnIndex < sectionLastColumnIndex; sectionFirstColumnIndex++) {
-			if(solved[sectionFirstRowIndex][sectionFirstColumnIndex][1].indexOf(number) > -1){
-				checked++;
-				checkedRow = sectionFirstRowIndex;
-				checkedColumn = sectionFirstColumnIndex;
+}
+
+function isSingle(){}
+
+function isHiddenSingle(row,col){
+	isHiddenSingleRow(row,col);
+	isHiddenSingleCol(row,col);
+	isHiddenSingleSection(row,col);
+}
+
+function isHiddenSingleRow(row,col){
+	let count;
+	for (let a = 0, length = solved[row][col][1].length; a < length; a++){
+		count = 0;
+		for (let x = 0; x<9; x++) {
+			if (solved[row][x][1].indexOf(solved[row][col][1][a]) > -1) count++;
+		}
+		if (count === 1) {
+			addSolution(row, col, solved[row][col][1][a]);
+			updateSuggestions(row, col, solved[row][col][0]);
+			break;
+		}
+	}
+}
+
+function isHiddenSingleCol(row,col){
+	let count;
+	for (let a = 0, length = solved[row][col][1].length; a < length; a++){
+		count = 0;
+		for (let x = 0; x<9; x++) {
+			if (solved[x][col][1].indexOf(solved[row][col][1][a]) > -1) count++;
+		}
+		if (count === 1) {
+			addSolution(row, col, solved[row][col][1][a]);
+			updateSuggestions(row, col, solved[row][col][0]);
+			break;
+		}
+	}
+}
+
+function isHiddenSingleSection(row,col){
+	let sectionFirstRowIndex = (row/3|0)*3;
+	let sectionLastRowIndex = sectionFirstRowIndex + 3;
+	let sectionFirstColumnIndex = (col/3|0)*3;
+	let sectionLastColumnIndex = sectionFirstColumnIndex + 3;
+	let count;
+	for (let a = 0, length = solved[row][col][1].length; a < length; a++) {
+		count = 0;
+		for (let i = sectionFirstRowIndex; i < sectionLastRowIndex; i++) {
+			for (let j = sectionFirstColumnIndex; j < sectionLastColumnIndex; j++) {
+				if (solved[i][j][1].includes(solved[row][col][1][a])) count++;
 			}
 		}
+		if (count === 1) {
+			addSolution(row, col, solved[row][col][1][a]);
+			updateSuggestions(row, col, solved[row][col][0]);
+			break;
+		}
 	}
-
-
-
-
-
-	if (checked === 1) {
-		solved[checkedRow][checkedColumn][0] = number;
-		solved[checkedRow][checkedColumn][1] = [];
-		updateSuggestionsRow(checkedRow, solved[checkedRow][checkedColumn][0]);
-		updateSuggestionsColumn(checkedColumn, solved[checkedRow][checkedColumn][0]);
-
-	}
-
-
 
 }
 
-function updateSuggestions() {
-	if (solvedNumbers < 81) solvedNumbers = 0;
+function isSolved(){
 	for ( let i = 0; i < 9; i++){
 		for (let j = 0; j < 9; j++){
-			if (solved[i][j][1].length == 1) solved[i][j][0] =  solved[i][j][1][0];
-
-			if (solved[i][j][0]) {
-				solved[i][j][1] = [];
-				solvedNumbers++;
-				updateSuggestionsColumn(j, solved[i][j][0]);
-				updateSuggestionsRow(i, solved[i][j][0]);
-				
-
-			} 
+			if (solved[i][j][2] === 'unsolved') return false;			
 		}
 	}	
-	for ( let i = 0; i < 9; i++){
-		for (let j = 0; j < 9; j++){
-			if (solved[i][j][1].length == 1) solved[i][j][0] =  solved[i][j][1][0];
-			if (solved[i][j][0]) {
-				solved[i][j][1] = [];
-				updateSuggestionsSection(i, j, solved[i][j][0]);
-
-			} 
-		}
-	}	
+	return true;
 }
+
+function isError(){
+	for ( let i = 0; i < 9; i++){
+		for (let j = 0; j < 9; j++){
+			if (solved[i][j][2] === 'unsolved' && solved[i][j][1].length === 0) return true;			
+		}
+	}	
+	return false;	
+}
+
+
+
+
 
 function output() {
 	for ( let i = 0; i < 9; i++){
@@ -125,6 +184,5 @@ function output() {
 }
 
 
-
-
 }
+
